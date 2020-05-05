a7f55d3f3f0ea8fb524207f1661c9f289911a0fa
panaceata <milinda.nan@panaceata.com>
2020-05-05 09:20:07 +0530

    facade  changes

--- Facade Problem/Report.java
+++ Facade Problem/Report.java
@@ -1,11 +1,37 @@
-public interface Report {
-    public void setHeader();
+class Report {
+    String hearder;
+    String footer;
+    String data;
 
-    public void setData();
+    public void setHeader() {
+        System.out.println("Set Header");
+    }
 
-    public void setFooter();
+    public void setData() {
+        System.out.println("Set Data");
+    }
 
-    public void printPDF();
+    public void setFooter() {
+        System.out.println("Set Footer");
+    }
 
-    public void printHTML();
+    public void printPDF() {
+        System.out.println("Print PDF");
+    }
+
+    public void printHTML() {
+        System.out.println("Print HTML");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        final Report report = new Report();
+
+        report.setHeader();
+        report.setData();
+        report.setFooter();
+        report.printPDF();
+        report.printHTML();
+    }
 }
