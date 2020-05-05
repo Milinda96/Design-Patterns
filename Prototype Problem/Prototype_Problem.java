c04c5aa89c0b97633e4bd45bab32f36255793de7
panaceata <milinda.nan@panaceata.com>
2020-05-05 10:40:29 +0530

    prototype problem changes

--- Prototype Problem/Prototype_Problem.java
+++ Prototype Problem/Prototype_Problem.java
@@ -1,24 +1,14 @@
-import java.util.HashMap;
-import java.util.Map;
-
-abstract class Button implements Cloneable {
+class Button {
 
     protected String color;
     protected String name;
 
-    abstract void setColor();
-
-    abstract void setName();
-
-    public Object clone() {
-        Object clone = null;
-        try {
-            clone = super.clone();
-        } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
-        }
-        return clone;
+    public void setColor() {
     }
+
+    public void setName() {
+    }
+
 }
 
 class Button_Prototype1 extends Button {
@@ -28,12 +18,12 @@ class Button_Prototype1 extends Button {
     }
 
     @Override
-    void setColor() {
+    public void setColor() {
         System.out.println("Button Prototype1 Color: " + this.color);
     }
 
     @Override
-    void setName() {
+    public void setName() {
         System.out.println("Button Prototype1 Name: " + this.name);
     }
 
@@ -47,42 +37,26 @@ class Button_Prototype2 extends Button {
     }
 
     @Override
-    void setColor() {
+    public void setColor() {
         System.out.println("Button Prototype2 Color: " + this.color);
     }
 
     @Override
-    void setName() {
+    public void setName() {
         System.out.println("Button Prototype2 Name: " + this.name);
-    }
-}
-
-class ButtonStore {
-
-    private static Map<String, Button> buttonMap = new HashMap<String, Button>();
-
-    static {
-        buttonMap.put("Light Blue", new Button_Prototype1());
-        buttonMap.put("Click1", new Button_Prototype1());
-        buttonMap.put("Gray", new Button_Prototype2());
-        buttonMap.put("Click2", new Button_Prototype2());
-    }
-
-    public static Button getColor(String color) {
-        return (Button) buttonMap.get(color).clone();
-    }
-
-    public static Button getName(String name) {
-        return (Button) buttonMap.get(name).clone();
     }
 }
 
 class Prototype {
     public static void main(String[] args) {
-        ButtonStore.getColor("Light Blue").setColor();
-        ButtonStore.getName("Click1").setName();
-        ButtonStore.getColor("Gray").setColor();
-        ButtonStore.getName("Click2").setName();
+
+        Button button_Prototype1 = new Button_Prototype1();
+        Button button_Prototype2 = new Button_Prototype2();
+
+        button_Prototype1.setColor();
+        button_Prototype1.setName();
+        button_Prototype2.setColor();
+        button_Prototype2.setName();
 
     }
 }
