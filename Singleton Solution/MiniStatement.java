8a542ae78497a863cc218b64ec0222ba99012de5
panaceata <milinda.nan@panaceata.com>
2020-05-06 23:52:53 +0530

    add MiniStatement class

---
+++ Singleton Solution/MiniStatement.java
@@ -0,0 +1,7 @@
+public class MiniStatement implements StatementType {
+    @Override
+    public String print() {
+        System.out.println("Mini Statement Created");
+        return "Mini Statement";
+    }
+}
