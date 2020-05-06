b1d24f5ce7137f981800c5b53d3304bd07dfc1d8
panaceata <milinda.nan@panaceata.com>
2020-05-06 23:52:06 +0530

    add DetailedStatement class

---
+++ Singleton Solution/DetailedStatement.java
@@ -0,0 +1,7 @@
+public class DetailedStatement implements StatementType {
+    @Override
+    public String print() {
+        System.out.println("Detailed Statement Created");
+        return "Detailed Statement";
+    }
+}
